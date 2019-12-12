3608fb4bae414e87b32e4b5116de8c54b40c6f1d
Ashish Awasthi <ashish.awasthi2@harman.com>
2019-12-12 16:53:38 +0530

    #0001 Adding base sample application

---
+++ AccesWifiInfo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AccesWifiInfo
+//
+//  Created by Ashish Awasthi on 12/12/19.
+//  Copyright © 2019 Tanuja Awasthi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
